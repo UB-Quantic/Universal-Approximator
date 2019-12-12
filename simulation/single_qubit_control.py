a5e6563e21fdb2c078dc9923dd5aa606fc808d81
dQuantic <david.lopez@bsc.es>
2019-12-12 18:39:30 +0100

    Commented code and small refactoring

--- simulation/single_qubit_control.py
+++ simulation/single_qubit_control.py
@@ -25,43 +25,60 @@ class SingleQubitControl(ABC):
     A class that controls the qubit
     """
     def __init__(self, meas_object, pulse_type = "ARBITRARY"):
+        
         self._meas_object = meas_object
         self._pulse_type = pulse_type
 
+        # Prepares calibration measurement to be done
         relPath = os.path.dirname(os.path.abspath(__file__))
         self._calibration_meas = ScriptTools.MeasurementObject(
                 os.path.join( relPath, self._calibration_file),
                 os.path.join( relPath, self._calibration_file_result ) )
 
+        # Basic initialization
         self._w, self._phi_0, self._A, self._c = 0,0,0,0
         self._accumulated_z_phase, self._next_pulse = 0,1 # initialize variables
 
     
     def calibrate(self):
-
+        """
+        Calibrate both the qubit pulses and the qubit measurement
+        """
+        # Perform calibration measurement
         ( t_cal, y_cal ) = self._calibration_meas.performMeasurement()
+        
+        # Adjust oscillations to cosinus and retrieve values
         self._w, self._phi_0, self._A, self._c = self._fit_cosinus( t_cal, y_cal )
     
-    @abstractmethod
     def process_result(self, result):
-        pass
+        """
+        Process result and convert it into P_0
+        """
+        P0 = ( result - (self._c - self._A) ) / (2 * self._A)
+        return P0
 
     
     def _fit_cosinus(self, t_cal, y_cal):
+        """
+        Fit the calibration result to a general cosinus
+        """
+        # First some trials are obtained so that the fit is succesful
         max_y = np.amax(y_cal)
         min_y = np.amin(y_cal)
         A_trial = abs(max_y - min_y) / 2
         c_trial = (max_y + min_y) / 2
 
+        # w and phi_0 are kind of known in advance
         param_ini = [25e6 * 2*np.pi, 0.1, A_trial, c_trial]
         parameters, par_errors = curve_fit( general_cosinus, t_cal, y_cal, param_ini)
 
+        # mapping parameters
         w = parameters[0]
         phi = parameters[1]
         A = parameters[2]
         c = parameters[3]
 
-        # # Plot results
+        # Plot results in case you want to be sure of the calibration
         # plt.figure(1)
         # plt.plot(t_cal,y_cal)
         # y_sim = []
@@ -75,12 +92,13 @@ class SingleQubitControl(ABC):
         # plt.plot(t_cal, y_sim)
         # plt.plot(t_cal, y_trial)
         # plt.show()
-
-
         return w, phi, A, c
 
 
     def _get_time_for_angle(self, angle):
+        """
+        Obtain the pulse time for a desired angle rotation
+        """
         
         if angle < self._phi_0:
             angle = angle + 2*np.pi
@@ -90,6 +108,9 @@ class SingleQubitControl(ABC):
 
 
     def add_x_gate(self, angle):
+        """
+        Add an x gate with specific angle to the sequence
+        """
         pulse_plat_name = "Control Pulse - Plateau #" + str(self._next_pulse)
         phase_plat_name = "Control Pulse - Phase #" + str(self._next_pulse)
 
@@ -100,6 +121,9 @@ class SingleQubitControl(ABC):
 
     
     def add_y_gate(self, angle):
+        """
+        Add an y gate with specific angle to the sequence
+        """
         pulse_plat_name = "Control Pulse - Plateau #" + str(self._next_pulse)
         phase_plat_name = "Control Pulse - Phase #" + str(self._next_pulse)
 
@@ -110,14 +134,25 @@ class SingleQubitControl(ABC):
 
 
     def add_z_gate(self, angle):
+        """
+        Add an z gate with specific angle to the sequence
+        """
+        # Z gates are actually virtual Z gates, so they are only accumulated phase
+        # in next x-y pulses
         self._accumulated_z_phase += np.rad2deg( angle )
 
     
     def finish_sequence(self):
+        """
+        Finish sequence so that it's ready for measurement
+        """
         self._meas_object.updateValue( "Control Pulse - # of pulses", self._next_pulse - 1 )
 
 
     def reset(self):
+        """
+        Reset parameters when a new sequence is started
+        """
         self._meas_object.updateValue( "Control Pulse - # of pulses", 0 )
         self._next_pulse = 1
         
@@ -125,25 +160,21 @@ class SingleQubitControl(ABC):
 
 class SQCSim(SingleQubitControl):
     """
-    A class that controls the qubit
+    A class that controls the qubit when in simulation mode
     """
-    def __init__(self, meas_object, pulse_type = "ARBITRARY", measurement_type = MEAS_TYPE_SIMULATION):
+    def __init__(self, meas_object, pulse_type = "ARBITRARY"):
 
-
+        # Initialize the single qubit control object with proper calibration
+        # files
         self._calibration_file = SIM_CAL_TEMPLATE
         self._calibration_file_result = SIM_CAL_RESULT
-
         SingleQubitControl.__init__(self, meas_object, pulse_type)        
-
-        
-    def process_result(self, result):
-        P0 = ( result - (c - A) ) / (2 * A) 
 
 
 
 class SQCExp(SingleQubitControl):
     """
-    A class that controls the qubit
+    A class that controls the qubit when in experiment mode
     """
     def __init__(self, meas_object, pulse_type = "ARBITRARY"):
         
@@ -151,15 +182,12 @@ class SQCExp(SingleQubitControl):
         self._calibration_file_result = EXP_CAL_RESULT
         
         SingleQubitControl.__init__(self, meas_object, pulse_type)        
-
-        
-    def process_result(self, result):
-        P0 = ( result - (self._c - self._A) ) / (2 * self._A)
-        return P0
-
     
 
     def add_x_gate(self, angle):
+        """
+        Add an x gate with specific angle to the sequence
+        """
 
         pulse_amp_name = "Control Pulse - Amplitude #" + str(self._next_pulse)
         pulse_plat_name = "Control Pulse - Plateau #" + str(self._next_pulse)
@@ -178,6 +206,9 @@ class SQCExp(SingleQubitControl):
 
     
     def add_y_gate(self, angle):
+        """
+        Add an y gate with specific angle to the sequence
+        """
 
         pulse_amp_name = "Control Pulse - Amplitude #" + str(self._next_pulse)
         pulse_plat_name = "Control Pulse - Plateau #" + str(self._next_pulse)
@@ -195,17 +226,31 @@ class SQCExp(SingleQubitControl):
         self._seq_time += 10e-9 + 20e-9 + plateau # width + spacing + plateau
 
 
-
-
     def finish_sequence(self):
+        """
+        Finish sequence so that it's ready for measurement
+        """
         self._meas_object.updateValue( "Control Pulse - # of pulses", self._next_pulse )
 
+        self._add_measurement_pulse()
+        self._add_first_pulse_delay()
 
+
+    def _add_first_pulse_delay(self):
+        """
+        Caculate and add the first pulse delay
+        """
+        first_pulse_delay = self._start_time - self._seq_time + 10e-9 # add one width
+        self._meas_object.updateValue( "Control Pulse - First pulse delay", first_pulse_delay )
+
+    def _add_measurement_pulse(self):    
+        """
+        Add measurement pulse after the qubit pulse sequence
+        """
         pulse_amp_name = "Control Pulse - Amplitude #" + str(self._next_pulse)
         pulse_plat_name = "Control Pulse - Plateau #" + str(self._next_pulse)
         pulse_phase_name = "Control Pulse - Phase #" + str(self._next_pulse)
         pulse_output_name = "Control Pulse - Output #" + str(self._next_pulse)
-
 
 
         self._meas_object.updateValue( pulse_amp_name, .175 )
@@ -213,12 +258,10 @@ class SQCExp(SingleQubitControl):
         self._meas_object.updateValue( pulse_phase_name, 0 )
         self._meas_object.updateValue( pulse_output_name, 1 )
 
-        first_pulse_delay = self._start_time - self._seq_time + 10e-9 # add one width
-        self._meas_object.updateValue( "Control Pulse - First pulse delay", first_pulse_delay )
-
-    
-
     def reset(self):
+        """
+        Reset parameters when a new sequence is started
+        """
         self._meas_object.updateValue( "Control Pulse - # of pulses", 0 )
         self._next_pulse = 1
         self._seq_time = 0
@@ -226,13 +269,21 @@ class SQCExp(SingleQubitControl):
     
         
 class SQCFactory():
+    """
+    Factory class that creates the single qubit pulse object depending on 
+    the measurement and pulse type
+    """
 
     @staticmethod
     def get_single_qubit_controller(meas_object, meas_type, pulse_type = "ARBITRARY"):
+        """
+        Create a single qubit pulse object depending on the measurement 
+        and pulse type
+        """
         
         if meas_type == MEAS_TYPE_SIMULATION:
-            return SQCSim(meas_object, pulse_type, meas_type)
+            return SQCSim(meas_object, pulse_type)
         elif meas_type == MEAS_TYPE_EXPERIMENT:
-            return SQCExp(meas_object, pulse_type, meas_type)
+            return SQCExp(meas_object, pulse_type)
 
         
--- simulation/universal_approximator.py
+++ simulation/universal_approximator.py
@@ -7,9 +7,9 @@ from Labber import ScriptTools
 
 import single_qubit_control as sqc
 
+# Constants that will be used throughout the code
 MEAS_TYPE_SIMULATION = "SIMULATION"
 MEAS_TYPE_EXPERIMENT = "EXPERIMENT"
-
 
 SIM_MEAS_TEMPLATE = "Simulation.hdf5"
 SIM_MEAS_RESULT = "Simulation_Result.hdf5"
@@ -19,28 +19,42 @@ EXP_MEAS_RESULT = "Experiment_Result.hdf5"
 _RELPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-##### add single qubit control constants ####
-
-
 class UniversalApproximator():
+    """
+    Class that uses the qubit as a Universal Approximator.
+    
+    It can be used both in simulation and in real experiment.
+    """
 
     def __init__(self, n_layers=2, measurement_type="SIMULATION",\
                  single_qubit_control_type="ARBITRARY"):
+        """
+        Initialize the class by sending the measurement type and the number of layers
+        of the algorithm. If known, also the type of qubit control is sent
+        """
         
-        if n_layers not in [2,3,5]:
+        # Check if values are correct and some initializations
+        if n_layers not in range(1,6):
             print("Number of layers not supported")
             exit()
+        self._n_layers = n_layers
+        
+        if measurement_type not in [MEAS_TYPE_EXPERIMENT, MEAS_TYPE_SIMULATION]:
+            print("Measurement types not supported")
         
         if single_qubit_control_type != "ARBITRARY":
             print("Single Qubit control type not supported")
             exit()
-        
-        self._executer = _UnivApproxFactory.get_executer( n_layers, measurement_type )
 
-        self._n_layers = n_layers
+        # get the actual executer of the algorithm        
+        self._executer = _UnivApproxFactory.get_executer( n_layers, measurement_type )
 
 
     def update_param(self, p):
+        """
+        Update the parameters of the algorithm. This should be a 
+        (n_layers x 3) array
+        """
 
         if len(p) != self._n_layers or len(p[0]) != 3:
             print("Parameters don't have the proper length")
@@ -49,61 +63,103 @@ class UniversalApproximator():
         self._executer.update_param(p)
     
     def update_x(self,x):
+        """
+        Update the value of x to be evaluated
+        """
 
         self._executer.update_x(x)
 
     
     def run(self):
+        """
+        Run the experiment
+        """
+
         P0 = self._executer.run()
         return P0
 
 
 class _UnivApproxFactory():
+    """
+    Factory class for creating the Universal Approximator algorithm
+    executor depending on if it's a simulation or a experiment
+    """
 
     @staticmethod
     def get_executer( n_layers, measurement_type, ):
+        """
+        Returns the suitable executer depending on the measurement type
+        ( "SIMULATION" or "EXPERIMENT"). Number of layers must also be supplied.
+        """
         if measurement_type == MEAS_TYPE_EXPERIMENT:
-            return _UnivApproxExperiment(n_layers, "ARBITRARY")
+            return _UnivApproxExperiment("ARBITRARY")
         elif measurement_type == MEAS_TYPE_SIMULATION:
-            return _UnivApproxSimulation(n_layers, "ARBITRARY")
+            return _UnivApproxSimulation("ARBITRARY")
         
 
 class _UnivApproxExecuter(ABC):
+    """
+    Abstract class that implements most of the features
+    of the universal approximator executer
+    """
 
-    def __init__(self, n_layers, single_qubit_control_type):
-              
-        self._n_layers = n_layers
-
-        self._sqc.calibrate()
-
+    def __init__(self, single_qubit_control_type):
+        """
+        Initialize the basic parameters of the Universal Approximator Executer
+        """
         self._p = []
         self._x = 0
 
     def update_param(self, p):
+        """
+        Update the algorithm parameters
+        """
         self._p = p
     
     def update_x(self,x):
+        """
+        Update the x value to be evaluated
+        """
         self._x = x
 
     def _calc_theta(self, p0, p1, x):
+        """
+        Provide the parameters and the x value, it calculates the
+        corresponding theta: theta= p1 + p0 * x
+        """
         theta = p1 + p0 * x
         return theta
     
     def _create_sequence(self):
-        
+        """
+        Creates the sequence once the parameters and the x value has been
+        supplied
+        """
+        # First a reset is done to previous pulses
         self._sqc.reset()
+
+        # For each layer, the following pulse are added:
+        # Ry(p1 + p0*x) -> Rz(p2)
         for param in self._p:
             
             self._sqc.add_y_gate( \
                 self._calc_theta( param[0], param[1], self._x ))
             self._sqc.add_z_gate( param[2] )
+        
         self._sqc.finish_sequence()
 
     @abstractmethod
     def _convert_to_P0(self, result):
+        """
+        Method to be overriden from subclasses
+        """
         return result        
         
     def run(self):
+        """
+        Run the experiment
+        """
+
         self._create_sequence()
         result = self._measurement.performMeasurement()
         P0 = self._convert_to_P0(result)
@@ -111,39 +167,62 @@ class _UnivApproxExecuter(ABC):
 
 
 class _UnivApproxSimulation( _UnivApproxExecuter ):
+    """
+    Executer class of the universal approximator algorithm
+    when in simulation mode
+    """
 
-    def __init__(self, n_layers, single_qubit_control_type):
+    def __init__(self, single_qubit_control_type):
         
+        # Creates the Labber measurement object
         self._measurement = ScriptTools.MeasurementObject(
                         os.path.join( _RELPATH, SIM_MEAS_TEMPLATE),
                         os.path.join( _RELPATH, SIM_MEAS_RESULT ) )
+        
+        # Creates and calibrates the single qubit controller
         self._sqc = sqc.SQCFactory.get_single_qubit_controller( \
              self._measurement, meas_type = MEAS_TYPE_SIMULATION)
+        self._sqc.calibrate()
 
-        _UnivApproxExecuter.__init__(self, n_layers, single_qubit_control_type)
+        # Performs superclass initialization
+        _UnivApproxExecuter.__init__(self, single_qubit_control_type)
 
 
     def _convert_to_P0(self, result):
-        
+        """
+        Easily convert polarization to P0
+        """
         P0 = (1 + result[0])/2 
         return P0
 
 
 
 class _UnivApproxExperiment( _UnivApproxExecuter ):
+    """
+    Executer class of the universal approximator algorithm
+    when in experiment mode
+    """
 
-    def __init__(self, n_layers, single_qubit_control_type):
+    def __init__(selflayers, single_qubit_control_type):
         
+        # Creates the Labber measurement object
         self._measurement = ScriptTools.MeasurementObject(
                         os.path.join( _RELPATH, EXP_MEAS_TEMPLATE),
                         os.path.join( _RELPATH, EXP_MEAS_RESULT ) )
+
+        # Creates and calibrates the single qubit controller
         self._sqc = sqc.SQCFactory.get_single_qubit_controller( \
              self._measurement, meas_type = MEAS_TYPE_EXPERIMENT)
+        self._sqc.calibrate()
 
-        _UnivApproxExecuter.__init__(self, n_layers, single_qubit_control_type)
+
+        # Performs superclass initialization
+        _UnivApproxExecuter.__init__(self, single_qubit_control_type)
 
     def _convert_to_P0(self, result):
-        
+        """
+        Uses the single qubit control class to process the result
+        """
         P0 = self._sqc.process_result(np.abs(result[1]))
         return P0
 
